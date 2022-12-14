34219eae90e1c9c9aedbf81c92117c231e372e97
Andriy <andriy.andriyan1@gmail.com>
2022-12-15 05:14:33 +0700

    Add profile image and integrate vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,10 @@
 import tailwind from '@astrojs/tailwind';
-import react from '@astrojs/react';
+import vercel from '@astrojs/vercel/serverless';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react(), tailwind()]
+  integrations: [tailwind()],
+  output: 'server',
+  adapter: vercel(),
 });
