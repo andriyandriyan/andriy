2b6d8eb9f97d9333366d6fea48e267dae216873c
Andriy <andriy.andriyan1@gmail.com>
2022-12-17 06:25:38 +0700

    Change to static build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,7 @@
 import tailwind from '@astrojs/tailwind';
-import cloudflare from '@astrojs/cloudflare';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  adapter: cloudflare()
 });
