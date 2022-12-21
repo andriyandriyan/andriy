5017b041d39f40be08a65796e8976f0451e93c73
Andriy <andriy.andriyan1@gmail.com>
2022-12-21 07:05:36 +0700

    integration seo, sitemap, robots and compress

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,11 @@
 import react from '@astrojs/react';
+import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import { defineConfig } from 'astro/config';
+import compress from 'astro-compress';
+import robotsTxt from 'astro-robots-txt';
 
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  site: process.env.SITE_URL,
+  integrations: [tailwind(), react(), sitemap(), robotsTxt(), compress()]
 });
